5a284cdde0346455f8c2f0ff2641a272911409ac
ANKUSH Banerjeee <banerjeeankush184@gmail.com>
2022-06-21 02:39:26 +0530

    some more changes made to the site  more needed will do in a while

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import { useContext } from "react";
 import { themeContext } from "./Context";
+import About from "./components/Portfolio/About";
 function App() {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
@@ -27,7 +28,7 @@ function App() {
       <Experience />
       <Works />
       <Portfolio />
-     
+     <About/>
       <Contact />
       <Footer />
     </div>
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,18 +2,25 @@ import React, { useContext } from "react";
 import "./Portfolio.css";
 import { Swiper, SwiperSlide } from "swiper/react"
 import "swiper/css";
-import Sidebar from "../../img/sidebar.png";
-import Ecommerce from "../../img/ecommerce.png";
-import HOC from "../../img/hoc.png";
-import MusicApp from "../../img/musicapp.png";
+import mainp from "../../img/mainp.jpeg";
+import productsp from "../../img/productsp.jpeg";
+import productsad from "../../img/productsad.jpeg";
+import admin from "../../img/admin.jpeg";
+import userorder from "../../img/userorder.jpeg";
+import orderadm from "../../img/orderadm.jpeg";
+import users from "../../img/users.jpeg";
+import r from "../../img/r.jpeg";
+import cart from "../../img/cart.jpeg";
 import { themeContext } from "../../Context";
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
 const Portfolio = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
   return (
     <div className="portfolio" id="portfolio">
       {/* heading */}
-      <span style={{color: darkMode?'white': ''}}>Recent Projects</span>
+      <span style={{ color: darkMode ? 'white' : '' }}>Recent Projects</span>
       <span>Portfolio</span>
 
       {/* slider */}
@@ -24,18 +31,37 @@ const Portfolio = () => {
         className="portfolio-slider"
       >
         <SwiperSlide>
-          <img src={Sidebar} alt="" />
+          <img src={mainp} style={{ width: "50vw", height: "60vh" }} alt="" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={Ecommerce} alt="" />
+          <img src={productsp} style={{ width: "50vw", height: "60vh" }} />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={MusicApp} alt="" />
+          <img src={cart} style={{ width: "50vw", height: "60vh" }} alt="" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={HOC} alt="" />
+          <img src={userorder} style={{ width: "50vw", height: "60vh" }} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={admin} style={{ width: "50vw", height: "60vh" }} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={users} style={{ width: "50vw", height: "60vh" }} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={productsad} style={{ width: "50vw", height: "60vh" }} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={r} style={{ width: "50vw", height: "60vh" }} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={orderadm} style={{ width: "50vw", height: "60vh" }} alt="" />
         </SwiperSlide>
       </Swiper>
+      <div>
+        <h1>Slide to right → </h1>
+      </div>
+      
     </div>
   );
 };
